e0e32d8d73f0dcd7c6bd6a90f175e33962a630e3
Ken Wheeler <ken_wheeler@me.com>
2015-09-11 15:31:26 -0400

    Adding `Simulate` middleware

--- tests/component.jsx
+++ tests/component.jsx
@@ -8,8 +8,18 @@ export default class TestComponent extends React.Component {
 
   render(){
     return <body>
-      <div>{this.state.test}</div>
+      <div onClick={this.props.onClick}>{this.state.test}</div>
       <p className="box">found me!</p>
+      <button
+        type="button"
+        onClick={this.props.onClick}>
+        Click Me
+      </button>
+      <button
+        type="button"
+        onClick={this.props.onClick}>
+        Click Me
+      </button>
     </body>
   }
 }
