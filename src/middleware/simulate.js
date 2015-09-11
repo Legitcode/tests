c60d51635875e5a8a5b93277aff94cd4a17c4156
Ken Wheeler <ken_wheeler@me.com>
2015-09-11 15:31:26 -0400

    Adding `Simulate` middleware

---
+++ src/middleware/simulate.js
@@ -0,0 +1,11 @@
+import React from 'react'
+let { TestUtils } = React.addons
+
+export default function simulate(data){
+  let element = data.element ? this.helpers.elements[data.element] : this;
+  element = Array.isArray(element) ? element[0] : element;
+  TestUtils.Simulate[data.method].call(this,
+    element,
+    data.options || null
+  );
+}
