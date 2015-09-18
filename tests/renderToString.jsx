862dbaea6562af38ec8be3cf2b4b4f23da563aaa
Zach <zackify@gmail.com>
2015-09-17 23:27:38 -0700

    added renderToString

---
+++ tests/renderToString.jsx
@@ -0,0 +1,18 @@
+import Test from '../src/legit-tests'
+import {Find} from '../src/middleware'
+import { expect } from 'chai';
+
+import TestComponent from './component'
+
+describe('Render To String', () => {
+
+  it('should return the html of the component', () => {
+    Test(<TestComponent test="wow"/>)
+    .test(({component}) => {
+      expect(component).to.be.ok
+    })
+    .renderToString(string => {
+      expect(string).to.match(/Click Me/)
+    })
+  });
+});
